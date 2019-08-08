9f56be15ef710418e49d14a3d44df563d31e2cc5
rwadada <wadada.knct@gmail.com>
2019-08-08 23:54:07 +0900

    [update] update index.ts
    create json-server

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,25 @@
 import * as functions from 'firebase-functions';
+import * as jsonServer from 'json-server';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+server.use(function (request, response, next) {
+    if (request.method !== 'GET') {
+        request.method = 'GET'
+    }
+    next()
+})
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+export const mock = functions.https.onRequest(server);
